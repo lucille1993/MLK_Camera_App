500d1dd981d6503c732074cd3ff12c7d7ce1f84a
Lucille Campbell <83090104+lucille1993@users.noreply.github.com>
2021-12-19 13:56:53 -0500

    Add files via upload

--- src/main/java/com/example/cameramlk/MainActivity.kt
+++ src/main/java/com/example/cameramlk/MainActivity.kt
@@ -9,6 +9,7 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.label.ImageLabeling
 import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         // Build a button to launch app
         findViewById<ImageView>(R.id.imageView)
-        val button = findViewById<Button>(R.id.button).setOnClickListener {
+        findViewById<Button>(R.id.button).setOnClickListener {
             dispatchTakePictureIntent()
         }
     }
@@ -41,13 +42,19 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
             val imageBitmap = data?.extras?.get("data") as Bitmap
-            val imageView = findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
-            val imageForMlKit = InputImage.fromBitmap(imageBitmap,0)
+
+            findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
+
+            val imageMlKit = InputImage.fromBitmap(imageBitmap,0)
+
             val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
 
-            labeler.process(imageForMlKit)
+
+            labeler.process(imageMlKit)
                 .addOnSuccessListener { labels ->
+                   findViewById<TextView>(R.id.textView).text = labels.toString()
              Log.i("Lucy", "Successful")
                 for (label in labels) {
                     val text = label.text
